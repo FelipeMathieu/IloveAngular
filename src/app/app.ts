108b7892df793c466bf1e393390c73ca2a3c6651
Felipe Vignoli Mathieu <mathieufelipe@gmail.com>
2025-06-05 11:05:32 -0300

    :D
    Signed-off-by: Felipe Vignoli Mathieu <mathieufelipe@gmail.com>

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,29 +1,41 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Fetcher } from './core/fetcher';
 import { Observable } from 'rxjs';
 import { IPostResponse } from './common/post-response.interface';
 import { DataStore } from './core/data-store';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MatButtonModule, CommonModule],
+  imports: [
+    RouterModule,
+    MatButtonModule,
+    CommonModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected $PostList: Observable<IPostResponse[]>;
+  protected $Loading: Observable<boolean>;
+  protected displayedColumns: string[] = ['userId', 'title', 'body'];
 
   constructor(
     private readonly _fetcher: Fetcher,
     private readonly _dataStore: DataStore
   ) {
     this.$PostList = this._dataStore.$Posts;
+    this.$Loading = this._dataStore.$Loading;
   }
 
   public OnLoad() {
-    this._fetcher.GetPostData();
+    this._fetcher.GetPostData().subscribe();
   }
 
   public trackById(_: number, value: IPostResponse) {
